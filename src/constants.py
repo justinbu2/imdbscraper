79338f98aca72a8ad231a58109db2b10bfa1ecad
Justin Bu <justinbu2@berkeley.edu>
2018-04-19 21:08:03 -0400

    add sample output

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,6 @@ import os
 
 IMDB_HOMEPAGE = "https://www.imdb.com"
 SAMPLE_OUTPUT_DIR = os.path.realpath(
-    "{0}/../dummy-output".format(os.path.dirname(os.path.realpath(__file__))))
+    "{0}/../sample-output".format(os.path.dirname(os.path.realpath(__file__))))
 CACHED_SITES_DIR = os.path.realpath(
     "{0}/../cached-sites".format(os.path.dirname(os.path.realpath(__file__))))
